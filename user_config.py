7e9d7a758732b9d83932743d9cd7879d20d1d480
krishsharma0413 <krishsharma0413@gmail.com>
2024-06-22 15:00:05 +0530

    feat: setup user_config.py

--- user_config.py
+++ user_config.py
@@ -1,2 +1,24 @@
+"""
+author: krish sharma
+
+This file is to setup your tinder-for-reviews without much difficulties.
+
+For review_type = "image". Please make sure that it is TEXT data type
+and has either "relative path" of the images or BASE64 image data.
+"""
+
+# what all columns/headers does your sql data have?
 headers = ["id", "review_content", "positive",
            "negative", "neutral", "polarity"]
+
+
+# which column/header do you want to be shown on the review card?
+column_for_review = "review_content"
+review_type = "text" # options: "text" or "image" (support for videos soon)
+polarity_column = "polarity"
+
+# what does each swipe add value to? please make sure these columns are int
+# ["column name", "name showed on website buttons."]
+left_swipe = ["negative", "negative"]
+up_swipe = ["neutral", "neu"]
+right_swipe = ["positive", "positive"]
