857ea4d431a5e81e5fbbc5344053f033b9c1856b
krishsharma0413 <krishsharma0413@gmail.com>
2024-08-16 06:23:44 +0530

    NEW: user_config for better user-experience

--- user_config.py
+++ user_config.py
@@ -1,29 +1,35 @@
 """
-author: krish sharma
+author: krishsharma0413
 
-This file is to setup your tinder-for-reviews without much difficulties.
-
-For review_type = "image". Please make sure that it is TEXT data type
-and has either "relative path" of the images or BASE64 image data.
+This file is to setup your tinder-for-reviews in a more user-friendly manner.
 """
 
-DATABASE_PATH = "./database/feedback.db" # this is an example database.
+table_name = "TABLE NAME"
+primary_key = "PRIMARY KEY COLUMN NAME" # primary key of your database. If None, it will be auto generated.
+DATABASE_PATH = "PATH TO YOUR DATABASE" # this is an example database.
 
 # what all columns/headers does your sql data have?
-headers = ["id", "review_content", "positive",
-           "negative", "neutral", "polarity"]
+headers = []
 
 
 # which column/header do you want to be shown on the review card?
-column_for_review = "review_content"
-review_type = "text" # options: "text" or "image" (support for videos soon)
-polarity_column = "polarity"
+column_for_review = "please udpate this" # column name
+review_type = "text" # options: "text" (support for image, videos soon)
+
+# make sure this column data type supports floating point values
+polarity_column = "please update this" # if set to None, a new column will be created with the name "polarity"
 
 # what does each swipe add value to? please make sure these columns are int
-# ["column name", "name showed on website buttons."]
-left_swipe = ["negative", "negative"]
-up_swipe = ["neutral", "neu"]
-right_swipe = ["positive", "positive"]
+# polarity wise, right is positive (+1), up is neutral (+0), and left is negative (-1)
+left_swipe = ["", ""] # [column name, display name (label on the website)]
+up_swipe = ["", ""] # [column name, display name (label on the website)]
+right_swipe = ["", ""] # [column name, display name (label on the website)]
 
 # A list of tuple which has username and then password
-login_password = [("admin", "admin"), ("krish", "secretpassword")]
+users = [("admin", "admin")]
+
+# setup host here for the server.
+# future tests might require the testcase to run the server
+# so to make sure it runs on the same host, you can set it up here.
+host = "localhost"
+port = 5556
