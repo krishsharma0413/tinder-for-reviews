9ea34974aa61a7458acd9871b332612871c254b7
krishsharma0413 <krishsharma0413@gmail.com>
2024-08-16 06:15:43 +0530

    Update app.py to adapt to user_config

--- app.py
+++ app.py
@@ -10,51 +10,30 @@ from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse, File
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from config import cursor
-from user_config import headers
+from config import cursor, user_cursor
+from user_config import *
 
 app = fastapi.FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS users\
-        (username TEXT, hashedpassword TEXT, token TEXT)")
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS feedback\
-        (id int primary key, review_content TEXT, positive decimal(1,5), negative\
-            decimal(1,5), neutral decimal(1,5), polarity decimal(1,5))")
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS postinformation\
-        (id int, creation text, link text, source\
-            text, FOREIGN KEY (id) REFERENCES feedback(id))")
-cursor.connection.commit()
 
-
-@app.get("/", response_class=HTMLResponse)
-async def index(request: fastapi.Request):
-    # check request cookies
-    if request.cookies.get("token", None):
-        res = cursor.execute("SELECT * FROM users WHERE token = ?",
-                             (request.cookies.get("token"),)).fetchone()
-        if res:
-            return templates.TemplateResponse("approval.html", {"request": request})
-        else:
-            # delete cookie
-            response = RedirectResponse("/login")
-            response.delete_cookie("token")
-            return response
-    return templates.TemplateResponse("login.html", {"request": request})
-
-
-@app.post("/", response_class=HTMLResponse)
+@app.route("/", methods=["get", "post"])
 async def index_post(request: fastapi.Request):
     # check request cookies
-    if request.cookies.get("token", None):
-        res = cursor.execute("SELECT * FROM users WHERE token = ?",
+    if request.cookies.get("token"):
+        res = user_cursor.execute("SELECT * FROM users WHERE token = ?",
                              (request.cookies.get("token"),)).fetchone()
         if res:
-            return templates.TemplateResponse("approval.html", {"request": request})
+            return templates.TemplateResponse(
+                "approval.html",
+                {
+                    "request": request,
+                    "left": left_swipe[1],
+                    "right": right_swipe[1],
+                    "up": up_swipe[1]
+                }
+            )
         else:
             # delete cookie
             response = RedirectResponse("/login")
@@ -65,10 +44,9 @@ async def index_post(request: fastapi.Request):
 
 @app.post("/login", response_class=JSONResponse)
 async def login(request: fastapi.Request, username: Annotated[str, fastapi.Form()], password: Annotated[str, fastapi.Form()]):
-    res = cursor.execute("SELECT * FROM users WHERE username = ? AND hashedpassword = ?",
+    res = user_cursor.execute("SELECT * FROM users WHERE username = ? AND hashedpassword = ?",
                          (username, sha256(password.encode()).hexdigest())).fetchone()
     if res:
-        print(res)
         response = RedirectResponse("/")
         response.set_cookie("token", res[2])
         return response
@@ -89,11 +67,11 @@ async def logout(request: fastapi.Request):
 
 @app.get("/json-export", response_class=FileResponse)
 async def json_exp(request: fastapi.Request):
-    if request.cookies.get("token", None):
-        res = cursor.execute("SELECT * FROM users WHERE token = ?",
+    if request.cookies.get("token"):
+        res = user_cursor.execute("SELECT * FROM users WHERE token = ?",
                              (request.cookies.get("token"),)).fetchone()
         if res:
-            alldata = cursor.execute("SELECT * FROM feedback").fetchall()
+            alldata = cursor.execute(f"SELECT * FROM {table_name}").fetchall()
             with open(f"./exports/{res[2]}.zip", "w", encoding="utf-8") as _:
                 zipf = ZipFile(f"./exports/{res[2]}.zip", "w", ZIP_DEFLATED)
                 try:
@@ -103,7 +81,6 @@ async def json_exp(request: fastapi.Request):
                     rmtree(f"./{res[2]}/")
                     os.mkdir(f"./{res[2]}/")
                 if len(alldata) > 15_000:
-                    print(len(alldata))
                     for x in range(0, len(alldata), 15_000):
                         with open(f"./{res[2]}/{x}-{x+15_000}.json", "w", encoding="utf-8") as filemanager:
                             dump({"data": [dict(zip(headers, y)) for y in alldata[x:x+15_000]]}, filemanager, indent=4)
@@ -120,13 +97,13 @@ async def json_exp(request: fastapi.Request):
 
 @app.get("/get-reviews", response_class=JSONResponse)
 async def get_reviews(request: fastapi.Request):
-    if request.cookies.get("token", None):
-        res = cursor.execute("SELECT * FROM users WHERE token = ?",
+    if request.cookies.get("token"):
+        res = user_cursor.execute("SELECT * FROM users WHERE token = ?",
                              (request.cookies.get("token"),)).fetchone()
         if res:
             res = cursor.execute(
-                "SELECT * FROM\
-                    feedback WHERE positive = 0 AND negative = 0 AND neutral = 0"
+                f"SELECT {primary_key}, {column_for_review} FROM\
+                    {table_name} WHERE {left_swipe[0]} = 0 AND {right_swipe[0]} = 0 AND {up_swipe[0]} = 0"
                 ).fetchmany(10)
             return JSONResponse({"data": res})
     else:
@@ -134,25 +111,25 @@ async def get_reviews(request: fastapi.Request):
 
 @app.get("/undo-review", response_class=JSONResponse)
 async def undo_review(request: fastapi.Request, db_id: str, polarity: int):
-    if request.cookies.get("token", None):
-        res = cursor.execute("SELECT * FROM users WHERE token = ?",
+    if request.cookies.get("token"):
+        res = user_cursor.execute("SELECT * FROM users WHERE token = ?",
                              (request.cookies.get("token"),)).fetchone()
         if res:
             res = cursor.execute(
-                "SELECT * FROM feedback WHERE id = ?", (db_id,)).fetchone()
+                f"SELECT * FROM {table_name} WHERE id = ?", (db_id,)).fetchone()
             if res:
                 factor = 0
                 if polarity == 1:
                     cursor.execute(
-                        "UPDATE feedback SET positive = ? WHERE id = ?", (res[2]-1, db_id))
+                        f"UPDATE {table_name} SET {right_swipe} = ? WHERE id = ?", (res[2]-1, db_id))
                     factor = -1
                 elif polarity == -1:
                     cursor.execute(
-                        "UPDATE feedback SET negative = ? WHERE id = ?", (res[3]+1, db_id))
+                        f"UPDATE {table_name} SET {left_swipe} = ? WHERE id = ?", (res[3]+1, db_id))
                     factor = 1
                 elif polarity == 0:
                     cursor.execute(
-                            "UPDATE feedback SET negative = ? WHERE id = ?", (res[4]-1, db_id))
+                        f"UPDATE {table_name} SET {left_swipe} = ? WHERE id = ?", (res[4]-1, db_id))
                     factor = 0
                 else:
                     return JSONResponse({"message": "Invalid polarity"})
@@ -162,7 +139,7 @@ async def undo_review(request: fastapi.Request, db_id: str, polarity: int):
                 except ZeroDivisionError:
                     polarity = 0
                 cursor.execute(
-                    "UPDATE feedback SET polarity = ? WHERE id = ?", (polarity, db_id))
+                    f"UPDATE {table_name} SET {polarity} = ? WHERE id = ?", (polarity, db_id))
                 cursor.connection.commit()
                 return JSONResponse({"message": "done"})
     return JSONResponse({"message": "Unauthorized"})
@@ -170,33 +147,33 @@ async def undo_review(request: fastapi.Request, db_id: str, polarity: int):
 
 @app.get("/approve-review", response_class=JSONResponse)
 async def approve_review(request: fastapi.Request, db_id: str, polarity: int):
-    if request.cookies.get("token", None):
-        res = cursor.execute("SELECT * FROM users WHERE token = ?",
+    if request.cookies.get("token"):
+        res = user_cursor.execute("SELECT * FROM users WHERE token = ?",
                              (request.cookies.get("token"),)).fetchone()
         if res:
             res = cursor.execute(
-                "SELECT * FROM feedback WHERE id = ?", (db_id,)).fetchone()
+                f"SELECT {right_swipe[0]}, {left_swipe[0]}, {up_swipe[0]} FROM {table_name} WHERE id = ?", (db_id,)).fetchone()
             if res:
                 factor = 0
                 if polarity == 1:
                     cursor.execute(
-                        "UPDATE feedback SET positive = ? WHERE id = ?", (res[2]+1, db_id))
+                        f"UPDATE {table_name} SET {right_swipe[0]} = ? WHERE id = ?", (res[0]+1, db_id))
                     factor = 1
                 elif polarity == -1:
                     cursor.execute(
-                        "UPDATE feedback SET negative = ? WHERE id = ?", (res[3]-1, db_id))
+                        f"UPDATE {table_name} SET {left_swipe[0]} = ? WHERE id = ?", (res[1]-1, db_id))
                     factor = -1
                 elif polarity == 0:
                     cursor.execute(
-                        "UPDATE feedback SET neutral = ? WHERE id = ?", (res[4]+1, db_id))
+                        f"UPDATE {table_name} SET {up_swipe[0]} = ? WHERE id = ?", (res[2]+1, db_id))
                     factor = 0
                 else:
                     return JSONResponse({"message": "Invalid polarity"})
-                # polarity = (positive - negative) / (positive + negative + neutral)
-                polarity = (res[2] + res[3] + factor) / \
-                    (res[2] - res[3] + res[4] + 1)
+                # new polarity = (positive - negative + factor) / (positive + negative + neutral + 1)
+                polarity = (res[0] + res[1] + factor) / \
+                    (res[0] - res[1] + res[2] + 1)
                 cursor.execute(
-                    "UPDATE feedback SET polarity = ? WHERE id = ?", (polarity, db_id))
+                    f"UPDATE {table_name} SET {polarity_column} = ? WHERE id = ?", (polarity, db_id))
                 cursor.connection.commit()
                 return JSONResponse({"message": "Approved"})
             else:
